03e9611225717eef1c40de87f9880411eb5f4752
gsanthar <ganeshsanthar@gmail.com>
2016-04-03 20:36:49 -0300

    Adding generated java code and UML class diagram with java sterotypes
    Adding the first generation java code generated from UML class diagram
    after applying java stereotypes. This first generation code will only
    contain setters and getters in addition to place holder for the methods
    to be implemented

---
+++ D5_Code_Generation/First_Generated_Source_with_pseudo_javacode/Payment.java
@@ -0,0 +1,72 @@
+
+
+public class Payment {
+
+	/**
+	 * 
+	 */
+	private Integer PaymentID;
+	/**
+	 * 
+	 */
+	private Integer Amount;
+	/**
+	 * 
+	 */
+	private Traveler traveller;
+	/**
+	 * Getter of PaymentID
+	 */
+	public Integer getPaymentID() {
+	 	 return PaymentID; 
+	}
+	/**
+	 * Setter of PaymentID
+	 */
+	public void setPaymentID(Integer PaymentID) { 
+		 this.PaymentID = PaymentID; 
+	}
+	/**
+	 * Getter of Amount
+	 */
+	public Integer getAmount() {
+	 	 return Amount; 
+	}
+	/**
+	 * Setter of Amount
+	 */
+	public void setAmount(Integer Amount) { 
+		 this.Amount = Amount; 
+	}
+	/**
+	 * Getter of traveller
+	 */
+	public Traveler getTraveller() {
+	 	 return traveller; 
+	}
+	/**
+	 * Setter of traveller
+	 */
+	public void setTraveller(Traveler traveller) { 
+		 this.traveller = traveller; 
+	}
+	/**
+	 * 
+	 */
+	public void MakePayment() { 
+		// TODO Auto-generated method
+	 }
+	/**
+	 * 
+	 */
+	public void ModeofPayment() { 
+		// TODO Auto-generated method
+	 }
+	/**
+	 * 
+	 */
+	public void ViewPayment() { 
+		// TODO Auto-generated method
+	 } 
+
+}
